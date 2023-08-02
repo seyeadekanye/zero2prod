a8315a3887b71554108b3d14937823c9f9a15da3
Seye <seyeadekanye@gmail.com>
2023-08-01 21:16:46 -0400

    ran rust fmt

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,7 +1,7 @@
 use std::net::TcpListener;
 
 #[tokio::test]
-async fn health_check_works(){
+async fn health_check_works() {
     let address = spawn_app();
     let client = reqwest::Client::new();
     let response = client
