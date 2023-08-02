99d5446887201f069ba930a098c65472b4a738c4
Seye <seyeadekanye@gmail.com>
2023-08-01 21:12:32 -0400

    add first integration test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
-fn main() {
-    println!("Hello, world!");
+use zero2prod::run;
+use std::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    run(TcpListener::bind("127.0.0.1:8000").expect("Failed to start server"))?.await
 }
